dfcfc84d9684bc66d48e2ceee79bcefb1be89312
Nayan Jain <nayanj148@gmail.com>
2026-05-23 20:47:32 +0530

    last 2

--- multiapp/asgi.py
+++ multiapp/asgi.py
@@ -1,14 +1,15 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-import multi.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multiapp.settings")
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+import multi.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
