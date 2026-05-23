892196fe5969519090694773d241e68eca7d6132
Nayan Jain <nayanj148@gmail.com>
2026-05-23 20:10:39 +0530

    fixed asgi setting issue

--- multiapp/asgi.py
+++ multiapp/asgi.py
@@ -1,17 +1,20 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import multi.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multiapp.settings')
+# IMPORTANT
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "multiapp.settings"
+)
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-
     "http": django_asgi_app,
 
     "websocket": AuthMiddlewareStack(
